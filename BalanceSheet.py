c5758afd1c803e90850eb3a8e274e86c3b97800b
Alejandro Prieto <prieto@cm3p.de>
2019-11-02 15:27:08 +0100

    field search

--- BalanceSheet.py
+++ BalanceSheet.py
@@ -2,33 +2,92 @@
 # python3 BalanceSheet.py
 
 from bs4 import BeautifulSoup
+import re
+
+
 class BalanceSheet:
 
     TotalRevenue = dict()
     parsedHtml = None
-    items = None
+    items = dict()
     src = None
 
 
     def __init__(self, src):
 
         self.src = src
-        self.parsedHtml = BeautifulSoup(self.src.text, 'lxml')
-        self.items = self.parsedHtml.find_all("span")
+        self.parsedHtml = BeautifulSoup(self.src.text, 'html.parser')
 
-        self.items = self.parsedHtml.find(text='Total Revenue').find_next('span').getText()
+
+        #self.set_data("Total Revenue", "Weighted average shares outstanding", "span")
+        #self.set_data("Income Before Tax", "Weighted average shares outstanding", "span")
+        self.set_data("Total Other Income/Expenses Net", "Weighted average shares outstanding", "span")
+        #self.set_data("Total Other Income", "Weighted average shares outstanding", "span")
 
         print(self.items)
 
-        #print(self.parsedHtml)
-        #print(self.parsedHtml.prettify())
-
-        self.set_data("TotalRevenue")
 
 
-    def set_data(self, searchString):
-        for item in self.items:
-            print(item.getText())
+    def set_data(self, search_string, stop_string, html_tag):
+
+
+        search_string = search_string.replace("/", "\\/")
+        print("Search Str: %s" % search_string)
+        # item = self.parsedHtml.find(re.compile(search_string))
+        
+        item = self.parsedHtml.find(re.compile("^Total Other Income.Expenses Net"))
+        print(item.next_sibling)
+
+
+        values = []
+
+        while True:
+            try:
+                print (item)
+                item = item.find_next(html_tag)
+
+                value = item.getText().replace(',', '')
+
+                m = re.search("([^0-9,]+)", value)
+
+                if m is None:
+
+                    print(float(value))
+                    values.append(value)
+
+                else:
+                    self.items[search_string] = values
+
+                    if search_string != stop_string:
+                        search_string = value
+                        self.set_data(search_string, stop_string, html_tag)
+
+                    break
+
+
+            except Exception as e:
+                print("Unexpected error:", e)
+                break
+
+
+
+
+
+
+
+
+        # ttm = item.find_next(html_tag)
+        # ttm_1 = ttm.find_next(html_tag)
+        # ttm_2 = ttm_1.find_next(html_tag)
+        # ttm_3 = ttm_2.find_next(html_tag)
+        # ttm_4 = ttm_3.find_next(html_tag)
+        #
+        # self.items["ttm"] = ttm.getText()
+        # self.items["ttm_1"] = ttm_1.getText()
+        # self.items["ttm_2"] = ttm_2.getText()
+        # self.items["ttm_3"] = ttm_3.getText()
+        # self.items["ttm_4"] = ttm_4.getText()
+
 
 
     def get_data(self):
